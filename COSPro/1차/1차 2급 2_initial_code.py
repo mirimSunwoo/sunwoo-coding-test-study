c47bca32366d5450ed3ccbc2faca6e8e9c99e25a
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2022-01-14 00:55:40 +0900

    새벽에 달려보자고!

---
+++ COSPro/1차/1차 2급 2_initial_code.py
@@ -0,0 +1,22 @@
+#You may use import as below.
+#import math
+
+def solution(price, grade):
+    #Write code here.
+    answer = 0
+    return answer
+
+#The following is code to output testcase.
+price1 = 2500
+grade1 = "V"
+ret1 = solution(price1, grade1)
+
+#Press Run button to receive output.
+print("Solution: return value of the function is", ret1, ".")
+    
+price2 = 96900
+grade2 = "S"
+ret2 = solution(price2, grade2)
+
+#Press Run button to receive output.
+print("Solution: return value of the function is", ret2, ".")
