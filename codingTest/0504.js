0481cb51caf7ebcf0d2481dd6e919a4147cc4acd
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-05-09 18:00:50 +0900

    [UPDATE] sunwoo / 0504 제일 작은 수 제거하기

--- codingTest/0504.js
+++ codingTest/0504.js
@@ -1,15 +1,29 @@
+//테스트 케이스 2개 성공 1개 실패
+function arr_min(arr){
+    var sort_arr = arr.sort();
+    min = sort_arr[0];
+    console.log(min);
+    return min;
+}
 function solution(arr) {
-    min = 0;
-    var answer = [];
+    answer = [];
+    arr_min(arr);
     for(var i=0; i<arr.length; i++){
-        if(min<arr[i]){
-            min = arr[i];
+        if(arr[i] != min){
+            answer.push(arr[i]);
+        }else if(arr.length == 1){
+            answer.push(-1);
         }
     }
-    arr.pop();
-    if(arr.length==0){
-        arr.push(-1);
-        return arr;
+    return answer;
+}
+//다른사람의 풀이
+function solution(arr) {
+    var min = arr[0];
+    for(var i = 1; i < arr.length; i++) {
+        if(min > arr[i]) min = arr[i];
     }
-    return arr;
+    arr.splice(arr.indexOf(min), 1);
+    if(arr.length) return arr;
+    else return [-1];
 }
