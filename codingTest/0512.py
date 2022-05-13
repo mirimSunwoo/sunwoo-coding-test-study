1d37e139fead55bb839fb9f305a88e54c460c14c
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2022-05-14 01:08:16 +0900

    [0512] 콜라츠 추측

--- codingTest/0512.py
+++ codingTest/0512.py
@@ -13,3 +13,11 @@ def solution(num):
     if count >= 500 :
         return -1
     return count
+
+#다른사람의 풀이
+def collatz(num):
+    for i in range(500):
+        num = num / 2 if num % 2 == 0 else num*3 + 1
+        if num == 1:
+            return i + 1
+    return -1
